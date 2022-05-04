eae99cce28128612f877a00e4f3e42dcd422413e
d-imosemi <83042447+d-imosemi@users.noreply.github.com>
2022-05-04 22:18:14 +0100

    Update info.py

--- project1/info.py
+++ project1/info.py
@@ -4,6 +4,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.mail.yahoo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "jimosemi@yahoo.com"
-EMAIL_HOST_PASSWORD = "svdkqvfaskdhiqzi"
+EMAIL_HOST_USER = "" #your email
+EMAIL_HOST_PASSWORD = "" #your password to the email
 
